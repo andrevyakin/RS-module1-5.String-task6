a0bfac0d85f5e42986661faf75b6ee380b583ede
Андрей <andrevyakin@gmail.com>
2022-11-08 21:02:51 +0300

    add indexOf and substring

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 let userText = prompt("Введите текст").trim();
 const wordFromText = prompt("Введите слово из текста").trim();
+const indexOfWord = userText.indexOf(wordFromText);
+userText= userText.substring(0, indexOfWord);
+alert(`Результат: ${userText}`);
